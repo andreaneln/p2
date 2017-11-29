f2cb4c737493cebd22c4a4041e512101ceb94d2d
andreaneln <andreane.lanthiernadeau@gmail.com>
2017-11-28 19:30:33 -0800

    table is undefined

--- public/scripts/index2.js
+++ public/scripts/index2.js
@@ -7,10 +7,29 @@ var item = 0;
 function encodeUriAndQuotes(untrustedStr) {
     return encodeURI(String(untrustedStr)).replace(/'/g, '%27').replace(')', '%29');
 }
+
+function addItem(item) {
+    
+        var row = document.createElement('tr');
+        row.className = "tableRows";
+        var id = item && item.id;
+        if (id) {
+            row.setAttribute('data-id', id);
+        }
+    
+
+        row.innerHTML = "<td>"+item.value+"</td>";
+    
+        var table = document.getElementById('plans');
+        table.lastChild.appendChild(row);
+    }
     
 function loadItems() {
     xhrGet(REST_DATA, function(data) {        
         var receivedItems = data || [];
+
+        console.log(data);
+
         var items = [];
         var i;
         // Make sure the received items have correct format
@@ -32,4 +51,5 @@ function loadItems() {
 }
 
 //updateServiceInfo();
+addItem(item);
 loadItems();
